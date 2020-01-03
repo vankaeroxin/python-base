748c574a30fa2c47cdf2b2735771ad51b6fa81ae
s.bogdanov <vanka.eroxin97@gmail.com>
2020-01-03 07:47:45 +0300

    Ex 5 correct reverse -> for

--- less2ex5.py
+++ less2ex5.py
@@ -1,4 +1,5 @@
 while 1:
+    min = True
     r_list = [7, 5, 3, 3, 2]
     inp = input("Введите целочисленное значение или q для выхода: ")
     if inp == 'q':
@@ -10,9 +11,14 @@ while 1:
     if r_list.count(el):
         r_list.insert(r_list.index(el) + r_list.count(el), el)
     else:
-        r_list.append(el)
-    r_list.sort(reverse=True)
+        for i, j in enumerate(r_list):
+            if el > j:
+                r_list.insert(i, el)
+                min = False
+                break
+        if min:
+            r_list.append(el)
     print(f"Пользователь ввел число {el}. Результат:", end=' ')
     for i in r_list:
         print(f"{i}", end=' ')
-    print()
+    print('\n')
