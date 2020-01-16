708327eca4a6c365ac05efa4cb993fbe859ced62
Sergey <vanka.eroxin97@gmail.com>
2020-01-16 11:42:01 +0300

    Added docstrings

--- less3ex6.py
+++ less3ex6.py
@@ -1,11 +1,17 @@
 def int_func(s):
-    try:
-        for i in s:
-            if ord(i) not in range(97, 123):
-                raise ValueError
-        return s.title()
-    except TypeError:
-        raise ValueError
+    """
+    Функция принимает слово из маленьких латинских букв и возвращает его же, но с прописной первой буквой
+
+    s = 'word';
+    return 'Word'
+
+    Если слово содержит в себе другие символы, кроме маленьких латинских букв, вознакает исключение TypeError.
+    Обработка типов не производится и неправильные аргументы могут привести к ошибке.
+    """
+    for i in s:
+        if ord(i) not in range(97, 123):
+            raise TypeError
+    return s.title()
 
 
 while 1:
@@ -14,14 +20,14 @@ while 1:
         st = input('Введите слово из маленьких латинских букв: ')
         try:
             print(int_func(st))
-        except ValueError:
+        except TypeError:
             print('Некорректный ввод')
     elif t == '2':
         st_list = input('Введите несколько слов из маленьких латинских букв разделённых пробелом: ').split(' ')
         try:
             for el in st_list:
                 print(int_func(el), end=' ')
-        except ValueError:
+        except TypeError:
             print('Некорректный ввод')
         print()
     elif t == '0':
