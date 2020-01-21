33ba4f7381923208ee92bc46af8b7d223e70ec52
s.bogdanov <vanka.eroxin97@gmail.com>
2020-01-21 23:14:02 +0300

    Ex 6 done

---
+++ less5ex6.py
@@ -0,0 +1,17 @@
+with open('ex6.txt') as f:
+    f_list = f.readlines()
+    d = {}
+    for el in f_list:
+        sum_ = 0
+        print(el.split())
+        s = ''.join(el.split()[1:])
+        for ch in ['(', ')', '—']:
+            s = s.replace(ch, ' ')
+        for item in s.split():
+            try:
+                sum_ += int(item)
+            except ValueError:
+                continue
+        d[el.split()[0]] = sum_
+
+print(d)
