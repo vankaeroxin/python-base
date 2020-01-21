c1b04c0e05bf6c5ceb6987f6fe441329a9a7adee
s.bogdanov <vanka.eroxin97@gmail.com>
2020-01-21 21:46:28 +0300

    Ex 4 done

--- less5ex1.py
+++ less5ex1.py
@@ -1,6 +1,6 @@
-with open('less1.txt', 'w') as w:
+with open('ex2.txt', 'w') as w:
     pass
-with open('less1.txt', 'a') as f:
+with open('ex2.txt', 'a') as f:
     while 1:
         s = input('Введите строку для записи в файл: ')
         if not s:
--- less5ex2.py
+++ less5ex2.py
@@ -1,4 +1,4 @@
-with open('less2.txt') as f:
+with open('ex2.txt') as f:
     f_list = f.readlines()
     print(f'В файле {f.name} {len(f_list)} строк')
     for el in f_list:
